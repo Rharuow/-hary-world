967f516413d78a721b12cf39c1d77ad833eba8a5
Harysson <haryssonsoares@gmail.com>
2024-02-02 20:25:45 -0300

    improvement: add test to user service in listUser action

--- api/authentication/src/user/user.service.spec.ts
+++ api/authentication/src/user/user.service.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { PrismaService } from '@/prisma/prisma.service';
+import { Role, User } from '@prisma/client';
 
 describe('UserService', () => {
   let service: UserService;
+  let user: Pick<
+    User & { role: Pick<Role, 'name' | 'id'> },
+    'id' | 'name' | 'role'
+  >;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,5 +20,16 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('listUsers', () => {
+    it('should return an array of users', async () => {
+      const result = await service.listUsers();
+      jest.spyOn(service, 'listUsers').mockImplementation(async () => result);
+
+      user = result[0];
+
+      expect(await service.listUsers()).toBe(result);
+    });
   });
 });
