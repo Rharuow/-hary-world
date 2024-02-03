22fb7fe7e144e24885e378dc7c48f9f663a158eb
Harysson <haryssonsoares@gmail.com>
2024-02-02 22:29:17 -0300

    improvement: create test to define admin to test and adminController

--- api/authentication/src/user/admin/admin.controller.spec.ts
+++ api/authentication/src/user/admin/admin.controller.spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaService } from '@/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 describe('AdminController', () => {
   let controller: AdminController;
+  let prismaService: PrismaService;
+  let admin: Prisma.UserGetPayload<{ include: { admin: true; role: true } }>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +16,22 @@ describe('AdminController', () => {
     }).compile();
 
     controller = module.get<AdminController>(AdminController);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
+    const role = await prismaService.role.findUnique({
+      where: { name: 'ADMIN' },
+    });
+    admin = await prismaService.user.create({
+      data: {
+        name: 'Admin test',
+        password: '123',
+        roleId: String(role?.id),
+      },
+      include: { role: true, admin: true },
+    });
+    expect(admin).toBeDefined();
     expect(controller).toBeDefined();
   });
 });
