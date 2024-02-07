093defc95d0c03b6bdfc896ce402d1af5e820088
Harysson <haryssonsoares@gmail.com>
2024-02-06 23:33:22 -0300

    feat: ui header root layout

---
+++ frontend/plataform/src/app/header.tsx
@@ -0,0 +1,18 @@
+import { Image } from "@/components/ui/image";
+
+import { MoreVertical } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <div className="bg-primary px-6 py-3 rounded-b-lg shadow-sm flex items-center">
+      <Image alt="logo" src="/logo.png" className="h-8 w-auto" />
+      <div className="flex flex-col grow">
+        <p className="text-center text-foreground font-semibold">Hary World</p>
+        <p className="text-center text-foreground font-semibold text-xs">
+          Desenvolvimento web
+        </p>
+      </div>
+      <MoreVertical size={24} />
+    </div>
+  );
+};
--- frontend/plataform/src/app/page.tsx
+++ frontend/plataform/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="min-h-screen bg-primary"></main>;
+  return <main></main>;
 }
