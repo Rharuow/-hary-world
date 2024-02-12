161da9cdaf2a7e720f4bf13c0bb743c67c5f4b5c
Harysson <haryssonsoares@gmail.com>
2024-02-11 23:30:09 -0300

    improvement: add lottie to creating new projects

--- frontend/plataform/src/app/products/page.tsx
+++ frontend/plataform/src/app/products/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+import Lottie from "lottie-react";
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Card } from "@/components/ui/card";
 import { Image } from "@/components/ui/image";
+
+import building from "@/../public/animation/building.json";
 
 import t from "@/i18n.json";
 
@@ -13,7 +17,7 @@ export default function Products() {
   return (
     <div className="flex flex-col px-4 py-6">
       <Accordion type="single" collapsible>
-        <AccordionItem value="caf" className="bg-primary">
+        <AccordionItem value="caf" className="bg-primary mb-3">
           <AccordionTrigger className="px-3">
             <Image
               alt="caf's logo"
@@ -25,6 +29,17 @@ export default function Products() {
           <AccordionContent className="px-3">
             <p className="font-semibold text-foreground text-justify">
               {t["pt-BR"].products.CAF.description}
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="building" className="bg-primary">
+          <AccordionTrigger className="px-3" withIcon={false}>
+            <Lottie animationData={building} className="h-[150px]" />
+          </AccordionTrigger>
+          <AccordionContent className="px-3">
+            <p className="text-foreground font-semibold text-center">
+              {t["pt-BR"].products.building.description}
             </p>
           </AccordionContent>
         </AccordionItem>
