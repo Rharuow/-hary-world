31529e7d9481e5fcf30071a21a773b596abf3b73
Harysson <haryssonsoares@gmail.com>
2024-02-14 11:46:44 -0300

    improvement: add button to see caf example

--- frontend/plataform/src/app/dashboard/page.tsx
+++ frontend/plataform/src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { BuildAnimation } from "@/components/buildAnimation";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+import t from "@/i18n.json";
+
 export default function Dashboard() {
   const session = cookies().get("session");
 
@@ -11,7 +13,7 @@ export default function Dashboard() {
     <div className="translate-y-full flex flex-col gap-6">
       <BuildAnimation />
       <p className="text-foreground text-center">
-        Estamos construindo essa página
+        {t["pt-BR"].general["We are building this page"]}
       </p>
     </div>
   );
