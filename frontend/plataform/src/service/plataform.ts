868a0fb252e5244d041d4f1492e04505c5590bcb
Harysson <haryssonsoares@gmail.com>
2024-02-18 19:39:16 -0300

    fix: base url from portal

--- frontend/plataform/src/service/plataform.ts
+++ frontend/plataform/src/service/plataform.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const plataformApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_PORTAL_URL + "api/v1/",
+  baseURL: process.env.NEXT_PUBLIC_PORTAL_URL + "/api/v1/",
 });
 
 export { plataformApi };
