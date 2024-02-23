3db936b66711f470ae6528663ff30db0a977d9a9
Harysson <haryssonsoares@gmail.com>
2024-02-22 21:03:25 -0300

    chore: delete unused files

--- frontend/caf/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-[radial-gradient(circle_at_top_left,_var(--tw-gradient-stops))] from-secondary to-primary"></main>
-  );
-}
