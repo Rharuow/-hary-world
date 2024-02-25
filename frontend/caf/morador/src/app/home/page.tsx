4d3c9b84ec6471d133bfd9af6383c0c4910fd885
Harysson <haryssonsoares@gmail.com>
2024-02-24 21:50:50 -0300

    feat: create empty component and card visitant with this component

--- frontend/caf/morador/src/app/home/page.tsx
+++ frontend/caf/morador/src/app/home/page.tsx
@@ -1,11 +1,60 @@
-import { Header } from "@/components/domain/header";
-import { Image } from "@/components/ui/image";
 import React from "react";
+
+import { Header } from "@/components/domain/header";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Card } from "@/components/ui/card";
+import { Image } from "@/components/ui/image";
+import { Empty } from "@/components/empty";
+
+const visitants = [
+  {
+    name: "Visitante 1",
+    type: "pedestrian",
+    code: "123456",
+    available: true,
+  },
+  {
+    name: "Visitante 2",
+    type: "driver",
+    code: "123456",
+    available: true,
+  },
+  {
+    name: "Visitante 3",
+    type: "driver",
+    code: "123456",
+    available: false,
+  },
+  {
+    name: "Visitante 4",
+    type: "pedestrian",
+    code: "123456",
+    available: false,
+  },
+];
 
 export default function Home() {
   return (
     <main className="min-h-svh relative bg-primary from-secondary to-primary flex flex-col gap-8">
       <Header />
+
+      <div className="flex flex-col px-3 z-10">
+        <Card className="p-3 bg-secondary">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="visitants">
+              <AccordionTrigger>Visitantes</AccordionTrigger>
+              <AccordionContent>
+                <Empty text="Nenhum visitante cadastrado!" />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </Card>
+      </div>
 
       <Image
         alt="sprite decoration"
