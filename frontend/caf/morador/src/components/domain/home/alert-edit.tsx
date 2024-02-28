a26f4a53878bf1c48b099fee27577bab0ebbadbd
Harysson <haryssonsoares@gmail.com>
2024-02-28 19:18:24 -0300

    improvement: create form component

--- frontend/caf/morador/src/components/domain/home/alert-edit.tsx
+++ frontend/caf/morador/src/components/domain/home/alert-edit.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useForm, useWatch } from "react-hook-form";
+import { FormProvider, useForm, useWatch } from "react-hook-form";
 import { Switch } from "@/components/ui/switch";
 import {
   Select,
@@ -23,6 +23,12 @@ import {
 } from "@/components/ui/select";
 import { Car, Footprints } from "lucide-react";
 import { SelectGroup } from "@radix-ui/react-select";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 
 export const AlertEdit = ({
   visitant,
@@ -31,7 +37,7 @@ export const AlertEdit = ({
   visitant: IVisitant;
   handleEdit: (visitant: IVisitant) => void;
 }) => {
-  const { register, control, setValue } = useForm<
+  const methods = useForm<
     Pick<IVisitant, "available" | "name" | "type" | "cpf">
   >({
     defaultValues: {
@@ -41,6 +47,8 @@ export const AlertEdit = ({
       cpf: visitant.cpf,
     },
   });
+
+  const { register, control, setValue } = methods;
 
   const availableWatch = useWatch({ control, name: "available" });
 
@@ -55,39 +63,64 @@ export const AlertEdit = ({
           <span className="text-secondary font-bold">Salvar</span>.
         </DialogDescription>
       </DialogHeader>
-      <div className="flex flex-col gap-4">
-        <Input label="Nome" {...register("name")} />
-        <div className="grid grid-cols-2 gap-3">
-          <div className="flex justify-center items-center gap-3">
-            <label htmlFor="available" className="text-white font-bold text-sm">
-              Ativo
-            </label>
-            <Switch
-              id="available"
-              checked={availableWatch}
-              onClick={() => setValue("available", !availableWatch)}
-              {...register("available")}
+      <FormProvider {...methods}>
+        <form className="flex flex-col gap-4">
+          <Input label="Nome" {...register("name")} />
+          <div className="grid grid-cols-2 gap-3">
+            <div className="flex justify-center items-center gap-3">
+              <label
+                htmlFor="available"
+                className="text-white font-bold text-sm"
+              >
+                Ativo
+              </label>
+              <Switch
+                id="available"
+                checked={availableWatch}
+                onClick={() => setValue("available", !availableWatch)}
+                {...register("available")}
+              />
+            </div>
+
+            <FormField
+              control={control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Tipo" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className="flex gap-1">
+                      <SelectItem
+                        value="pedestrian"
+                        className="flex justify-center"
+                      >
+                        <span className="flex justify-center items-center gap-2">
+                          <Footprints size={16} /> Pedestre
+                        </span>
+                      </SelectItem>
+                      <SelectItem
+                        value="driver"
+                        className="flex justify-center"
+                      >
+                        <span className="flex justify-center items-center gap-2">
+                          <Car size={16} /> Habilitado
+                        </span>
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
             />
           </div>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Tipo" />
-            </SelectTrigger>
-            <SelectContent className="flex gap-1" {...register("type")}>
-              <SelectItem value="pedestrian" className="flex justify-center">
-                <span className="flex justify-center items-center gap-2">
-                  <Footprints size={16} /> Pedestre
-                </span>
-              </SelectItem>
-              <SelectItem value="driver" className="flex justify-center">
-                <span className="flex justify-center items-center gap-2">
-                  <Car size={16} /> Pedestre
-                </span>
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+        </form>
+      </FormProvider>
       <DialogFooter>
         <Button
           className="text-primary-foreground font-bold focus:bg-primary-dark focus:text-white hover:bg-primary-dark hover:text-white"
